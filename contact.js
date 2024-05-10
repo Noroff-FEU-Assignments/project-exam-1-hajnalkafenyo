1b2a704af136bd322c7c77c3a6da290a6f884eff
hajnalkafenyo <77206669+hajnalkafenyo@users.noreply.github.com>
2024-05-10 18:11:29 +0200

    fix form validation

---
+++ contact.js
@@ -0,0 +1,102 @@
+function validateEmail(email) {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (reg.test(email) == false) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const emailField = document.getElementById('email');
+if (emailField) {
+    emailField.addEventListener('input', validateEmail);
+} else {
+    console.log('nincs');
+}
+
+const nameField = document.getElementById('contact-form')
+if (nameField) {
+    nameField.addEventListener("submit", submit);
+}
+
+function validateName(name) {
+    if (name.length > 5) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validateSubject(subject) {
+    if (subject.length > 15) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validateMessage(message) {
+    if (message.length > 25) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function submit(e) {
+    console.log('submit');
+    e.preventDefault();
+    const nameValue = document.getElementById('name').value;
+    const isNameValid = validateName(nameValue);
+    document.getElementById('nameError').innerText = (isNameValid ? '' : 'The name is too short');
+    const emailValue = document.getElementById('email').value;
+    const isEmailValid = validateEmail(emailValue);
+    document.getElementById('email-error').innerText = (isEmailValid ? '' : 'The email is not valid');
+    const subjectValue = document.getElementById('subject').value;
+    const isSubjectValid = validateSubject(subjectValue);
+    document.getElementById('subjectError').innerText = (isSubjectValid ? '' : 'The subject is too short');
+    const messageValue = document.getElementById('message').value;
+    const isMessageValid = validateMessage(messageValue);
+    document.getElementById('messageError').innerText = (isMessageValid ? '' : 'The message is too short');
+
+    if (isNameValid && isEmailValid && isMessageValid && isSubjectValid) {
+        document.getElementById('contact-form').style.display = 'none';
+        document.getElementById('thank-you-message').style.display = 'block';
+    }
+
+}
+
+
+
+
+
+
+/*
+function checkName(x) {
+    var x = document.myForm;
+    const input = x.name.value;
+    const errMsgHolder = document.getElementById('nameErrMsg');
+    if (input.length < 5) {
+        errMsgHolder.innerHTML =
+            'Please enter a name with at least 5 letters';
+        return false;
+    } else if (!(/^\S{3,}$/.test(input))) {
+        errMsgHolder.innerHTML =
+            'Name cannot contain whitespace';
+        return false;
+    } else if (!(/^[a-zA-Z]+$/.test(input))) {
+        errMsgHolder.innerHTML =
+            'Only alphabets allowed'
+    }
+    else if (!(/^(?:(\w)(?!\1\1))+$/.test(input))) {
+        errMsgHolder.innerHTML =
+            'per 3 alphabets allowed'
+    }
+    else {
+        errMsgHolder.innerHTML = '';
+        return undefined;
+    }
+
+}
+*/
